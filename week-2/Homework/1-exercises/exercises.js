2231726ab5ea687323ba1f24c634dbbb501469dc
Adrian <adriansiv97@hotmail.com>
2021-03-27 17:52:54 +0100

    Done the exercises and project

--- week-2/Homework/1-exercises/exercises.js
+++ week-2/Homework/1-exercises/exercises.js
@@ -13,9 +13,17 @@
  *      .....
  * </div>
  */
+let content = document.querySelector("#content");
 function exerciseOne(arrayOfPeople) {
-  let content = document.querySelector("#content");
-}
+  arrayOfPeople.forEach(p => {
+    const h1 = document.createElement("h1");
+    const h2 = document.createElement("h2");
+    h1.innerText = p.name;
+    h2.innerText = p.job;
+    content.appendChild(h1);
+    content.appendChild(h2);
+  })
+} 
 
 /**
  *
@@ -24,8 +32,36 @@ function exerciseOne(arrayOfPeople) {
  * All of your HTML should go inside the Div tag with the id "content".
  *
  */
+
+
 function exerciseTwo(shopping) {
-  //Write your code in here
+  const ul = document.createElement("ul");
+  shopping.forEach(item => {
+    const listItem = document.createElement("li");
+    listItem.textContent = item;
+    ul.appendChild(listItem);
+
+  })
+  content.appendChild(ul) 
+    
+
+  // books.forEach(obj => {
+  //   const bookTitle = obj.title;
+  //   const author = obj.author;
+  //   const read = obj.alreadyRead;
+  //   const li = document.createElement("li");
+  //   const ul = document.createElement("ul")
+  //   const p = document.createElement("p");
+  //   li.innerText = ul;
+  //   ul.innerText = p;
+  //   p.innerText = `${bookTitle} - ${author}`;
+  //   content.appendChild(p);
+  //   if (Object.values(read) === true){
+  //     document.p.style.backgroudColor = "green"
+  //   }else if (Object.values(read) === false){
+  //     document.p.style.backgroudColor = "red";
+  //   }
+  // })
 }
 
 /**
@@ -58,7 +94,19 @@ function exerciseTwo(shopping) {
     The end result should look something like this: https://hyf-js2-week1-makeme-ex1-demo.herokuapp.com/
 **/
 function exerciseThree(books) {
-  //Write your code in here
+ const ul = document.createElement("ul");
+ books.forEach(book => {
+   const li = document.createElement("li");
+   li.style.backgroundColor = book.alreadyRead ? "green" : "red"
+   const p = document.createElement("p");
+   const img = document.createElement("img");
+   p.textContent = book.title + " - " + book.author;
+   img.setAttribute("src", book.url);
+   li.appendChild(p);
+   li.appendChild(img);
+   ul.appendChild(li)
+ })
+ content.appendChild(ul)
 }
 
 //
@@ -87,17 +135,20 @@ const books = [
   {
     title: "The Design of Everyday Things",
     author: "Don Norman",
-    alreadyRead: false
+    alreadyRead: false,
+    url: "https://images-na.ssl-images-amazon.com/images/I/410RTQezHYL._SX326_BO1,204,203,200_.jpg"
   },
   {
     title: "The Most Human Human",
     author: "Brian Christian",
-    alreadyRead: true
+    alreadyRead: true,
+    url:"https://images-na.ssl-images-amazon.com/images/I/51iSxrqL+sL._SX335_BO1,204,203,200_.jpg"
   },
   {
     title: "The Pragmatic Programmer",
     author: "Andrew Hunt",
-    alreadyRead: true
+    alreadyRead: true,
+    url: "https://images-na.ssl-images-amazon.com/images/I/51cUVaBWZzL._SX380_BO1,204,203,200_.jpg"
   }
 ];
 
