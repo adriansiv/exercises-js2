5ab9bf82bbb0eb5c7b1b09a45e61769a8585e075
Adrian <adriansiv97@hotmail.com>
2021-04-07 15:28:46 +0200

    other part

--- week-3/Homework/1-alarmclock/alarmclock.js
+++ week-3/Homework/1-alarmclock/alarmclock.js
@@ -1,5 +1,25 @@
-function setAlarm() {}
+let timerID;
 
+function setAlarm() {
+  let alarmTime = document.querySelector("#alarmSet").valueAsNumber;
+  let text = document.querySelector("#timeRemaining");
+
+  let currentTime = alarmTime;
+  timerID = setInterval(function () {
+    if(currentTime === 0) {
+      clearInterval(timerID);
+      let bodyColor = document.querySelector("body");
+      bodyColor.style.backgroundColor = "red";
+      playAlarm();
+    }
+    if (currentTime >= 0) {
+      text.innerText = "Time Reamining: 00:" + currentTime;
+      console.log(currentTime)
+    }
+    currentTime = currentTime - 1;
+
+  }, 1000);
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
